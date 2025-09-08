11bca7e21d034cdbea5319948dd13c0579a4ef59
Sridharan M <112605186+Sridharan777@users.noreply.github.com>
2025-09-08 14:04:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1,
 )
 
-name, authentication_status, username = authenticator.login("Login", location="main")
+name, authentication_status, username = authenticator.login("Login", "main")
 
 if authentication_status:
     st.sidebar.write(f"Welcome {name}")
